94cb2447b81c8deca47176b98e2ff63cf4de8cc0
Emmanuel <emmanuel.effiong361@gmail.com>
2021-07-12 12:27:08 +0100

    worked on the input and header component

--- src/Container/ElementsContainer.js
+++ src/Container/ElementsContainer.js
@@ -1,8 +1,5 @@
 import styled from "styled-components";
 import bg1 from "../images/bg-desktop-light.jpg";
-import sun from "../images/icon-sun.svg";
-import moon from "../images/icon-moon.svg";
-
 
 export const Background = styled.div `
     background-image: url(${bg1});
@@ -12,16 +9,49 @@ export const Background = styled.div `
     background-size: cover;
 `
 export const MainCont = styled.div `
-
+    width:100%;
+    max-width:550px;
+    margin: auto;
 `
 export const Heading = styled.div`
     display:flex;
     justify-content:space-between;
-
+    margin-top: -40%;
+    color: hsl(0, 0%, 98%);
 `
 export const TitleHeading = styled.p`
-
+    font-family: 'Josefin Sans', sans-serif;
+    font-weight: 700;
+    font-size: 35px;
+    letter-spacing: 15px;  
 `
 export const LightChange = styled.img`
-
+    cursor:pointer;
+`
+export const Form = styled.form`
+    display:flex;
+    align-items:center;
+    background-color:hsl(0, 0%, 98%);
+    height:50px;
+    padding: 1% 5%;
+    border-radius:5px;
+    margin-top: 10%;
+`
+export const InputImg = styled.div`
+    border-radius:50%;
+    height:20px;
+    width:20px;
+    border:2px solid hsl(236, 33%, 92%);
+`
+export const Input = styled.input`
+    width: 85%;
+    height: 70%;
+    background-color: transparent;
+    border: 0;
+    font-size:20px;
+    font-family: inherit;
+    caret-color: hsl(220, 98%, 61%);
+    &:focus {
+        outline: none;
+    }
 `
--- src/Container/index.jsx
+++ src/Container/index.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
-import { Background, Heading, TitleHeading } from './ElementsContainer'
-import './container.css'
+import { Background, Heading, TitleHeading, LightChange, MainCont, Form, InputImg, Input } from './ElementsContainer'
+//import sun from "../images/icon-sun.svg";
+import moon from "../images/icon-moon.svg";
+//import icon from "../images/icon-check.svg";
 
 const Container = () => {
     return (
         <div>
             <Background />
-            <div>
+            <MainCont>
                 <Heading>
                     <TitleHeading>TODO</TitleHeading>
+                    <div>
+                    <LightChange 
+                        src={moon}
+                        alt="light-change"
+                    />
+                    </div>
                 </Heading>
-            </div>
+                <Form>
+                    <div style={{width:"10%"}}>
+                        <InputImg />
+                    </div>
+                    <Input />
+                </Form>
+            </MainCont>
         </div>
     )
 }
