dc69324689658ffac020fdcd2f042c37bd017853
Emmanuel <emmanuel.effiong361@gmail.com>
2021-07-13 23:14:31 +0100

    still building out the skeleton

--- src/Container/ElementsContainer.js
+++ src/Container/ElementsContainer.js
@@ -32,20 +32,20 @@ export const Form = styled.form`
     display:flex;
     align-items:center;
     background-color:hsl(0, 0%, 98%);
-    height:50px;
-    padding: 1% 5%;
+    height:60px;
+    padding: 0.7% 5%;
     border-radius:5px;
     margin-top: 10%;
 `
 export const InputImg = styled.div`
     border-radius:50%;
-    height:20px;
-    width:20px;
+    height:25px;
+    width:25px;
     border:2px solid hsl(236, 33%, 92%);
 `
 export const Input = styled.input`
     width: 85%;
-    height: 70%;
+    height: 80%;
     background-color: transparent;
     border: 0;
     font-size:20px;
@@ -54,4 +54,55 @@ export const Input = styled.input`
     &:focus {
         outline: none;
     }
+`
+export const ListCont = styled.div`
+    background-color:hsl(0, 0%, 98%);
+    border-radius:5px;
+    margin-top:30px;
+    
+`
+export const List = styled.div`
+    background-color: transparent;
+    font-size:20px;
+    font-family: inherit;
+    color: hsl(235, 19%, 35%);
+    flex: 1;
+    margin-left: 20px;
+    display: flex;
+    align-items:center;
+`
+export const Deletebtn = styled.img`
+    height: 18px;
+    width:18px;
+    margin-right:20px;
+    cursor: pointer; 
+    margin-left:45%;
+    /* display:${props => props.show ? "block" : "none"};  */
+
+`
+
+export const SingleList = styled.div`
+    display:flex;
+    align-items:center;
+    height: 60px;
+    justify-content: space-between;
+    border-bottom:1px solid hsl(236, 33%, 92%);
+`
+
+export const ListImg = styled(InputImg)`
+    margin-left: 50%;
+    position:relative;
+    cursor:pointer;
+    &:hover{
+        border: 1px solid hsl(220, 98%, 61%);
+        background:linear-gradient(to right, #57DDFF, #C058F3);
+    }
+`
+
+export const Check = styled.img`
+    height: 13px;
+    width:13px;
+    position:absolute;
+    top:6px;
+    left:5px;
 `
--- src/Container/index.jsx
+++ src/Container/index.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import { Background, Heading, TitleHeading, LightChange, MainCont, Form, InputImg, Input } from './ElementsContainer'
+import React,{useState} from 'react'
+import { Background, Heading, TitleHeading, LightChange, MainCont, Form, InputImg, Input, ListCont, List, ListImg, SingleList, Deletebtn, Check,  } from './ElementsContainer';
 //import sun from "../images/icon-sun.svg";
 import moon from "../images/icon-moon.svg";
-//import icon from "../images/icon-check.svg";
+import icon from "../images/icon-check.svg";
+import deleteicon from "../images/icon-cross.svg";
+
 
 const Container = () => {
+
+    // const [show, setShow] = useState(false);
+
+    // const showLeave = () => setShow(false)
+    // const showEnter = () => setShow(true)
+
+
     return (
         <div>
             <Background />
@@ -12,10 +21,10 @@ const Container = () => {
                 <Heading>
                     <TitleHeading>TODO</TitleHeading>
                     <div>
-                    <LightChange 
-                        src={moon}
-                        alt="light-change"
-                    />
+                        <LightChange 
+                            src={moon}
+                            alt="light-change"
+                        />
                     </div>
                 </Heading>
                 <Form>
@@ -24,6 +33,43 @@ const Container = () => {
                     </div>
                     <Input />
                 </Form>
+                <ListCont>
+                    <SingleList>
+                        <div style={{width:"10%"}}>
+                            <ListImg>
+                                <Check src={icon}/>
+                            </ListImg>
+                        </div>
+                        <List >
+                           <p>Jog Around the park 3x </p>
+                            <Deletebtn src={deleteicon}/>
+                        </List>
+                    </SingleList>
+                    <SingleList>
+                        <div style={{width:"10%"}}>
+                            <ListImg>
+                                <Check src={icon}/>
+                            </ListImg>
+                        </div>
+                        <List>
+                           <p>Pick up Groceries </p>
+                            <Deletebtn src={deleteicon}/>
+                        </List>
+                    </SingleList>
+                    <SingleList>
+                        <div style={{width:"10%"}}>
+                            <ListImg>
+                                <Check src={icon}/>
+                            </ListImg>
+                        </div>
+                        <List >
+                           <p>Practise Javascript </p>
+                            <Deletebtn src={deleteicon}/>
+                        </List>
+                    </SingleList>
+                    
+                </ListCont>
+
             </MainCont>
         </div>
     )
